71a890cbcba05cb7d282b45832ae6ff1eb1ba6ba
MotasemZiad <miziad2000@gmail.com>
2022-11-06 17:23:31 +0200

    queue

--- src/App.java
+++ src/App.java
@@ -1,5 +1,38 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        CustomQueue queue = new CustomQueue(8);
+        queue.enQueue(0);
+        queue.enQueue(2);
+        queue.enQueue(4);
+        queue.enQueue(19);
+        queue.enQueue(21);
     }
+}
+
+class CustomQueue {
+    private int arr[];
+    private int front;
+    private int rear;
+
+    CustomQueue(int size) {
+        arr = new int[size];
+        front = 0;
+        rear = -1;
+    }
+
+    void enQueue(int x) {
+        arr[++rear] = x;
+    }
+
+    int deQueue() {
+        int el = arr[front];
+        for (int i = 0; i < rear; i++) {
+            arr[i] = arr[i + 1];
+        }
+        rear--;
+        return el;
+    }
+
 }
