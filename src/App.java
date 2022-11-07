5fb67aa089753b4cbc3407f255e48a9a3ae0ce70
MotasemZiad <miziad2000@gmail.com>
2022-11-07 18:50:35 +0200

    Wrapper Classes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        boolean obj = new Boolean(true);
+        obj = false;
+        System.out.println(obj);
+
+        char ch = new Character('a');
+        System.out.println(ch);
+
+        long l = new Long((long) 100234L);
+        System.out.println(l);
+
+        int i = new Integer(202);
+        System.out.println(i);
+
+        double d = new Double(3.1492);
+        System.out.println(d);
     }
 }
