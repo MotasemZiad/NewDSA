181bcf104498119eca34d86ffce124bf87274abd
MotasemZiad <miziad2000@gmail.com>
2022-11-07 18:37:07 +0200

    playing around with Classes

--- src/App.java
+++ src/App.java
@@ -1,16 +1,36 @@
 import DataStructures.Linear.Dynamic.CustomQueue;
+import Dummy.Counter;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
 
-        CustomQueue queue = new CustomQueue(8);
-        queue.enQueue(0);
-        queue.enQueue(2);
-        queue.enQueue(4);
-        queue.enQueue(19);
-        queue.enQueue(21);
-        System.out.println(queue.deQueue());
-        System.out.println(queue.deQueue());
+        // CustomQueue queue = new CustomQueue(8);
+        // queue.enQueue(0);
+        // queue.enQueue(2);
+        // queue.enQueue(4);
+        // queue.enQueue(19);
+        // queue.enQueue(21);
+        // System.out.println(queue.deQueue());
+        // System.out.println(queue.deQueue());
+
+        Counter c;
+        c = new Counter();
+
+        c.increment();
+        c.increment(3);
+        int temp = c.getCount();
+        System.out.println(temp);
+        c.reset();
+        System.out.println(c.getCount());
+        Counter d = new Counter(5);
+        d.increment();
+        System.out.println(d.getCount());
+        Counter e = d;
+        temp = e.getCount();
+        System.out.println(temp);
+        e.increment(2);
+        System.out.println(e.getCount());
+        System.out.println(d.getCount());
     }
 }
