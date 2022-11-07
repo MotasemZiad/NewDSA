df05a6b09db71169941c1b7331d7f0831366059e
MotasemZiad <miziad2000@gmail.com>
2022-11-07 19:40:40 +0200

    Iterators

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 import Generics.Pair;
 
 public class App {
@@ -7,5 +9,16 @@ public class App {
         Pair<String, Integer> pair = new Pair<String, Integer>("ID", 3);
         System.out.println(pair.getFirst());
         System.out.println(pair.getSecond());
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("rahul");
+        list.add("jai");
+        // list.add(32);//compile time error  
+        String s = list.get(1);// type casting is not required  
+        System.out.println("element is: " + s);
+        Iterator<String> itr = list.iterator();
+        while (itr.hasNext())
+            System.out.println(itr.next());
+
     }
 }
