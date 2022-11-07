4a40735e6d25d7ae5a0d27edc1623286d46079a7
MotasemZiad <miziad2000@gmail.com>
2022-11-07 19:45:01 +0200

    write it by yourself

--- src/App.java
+++ src/App.java
@@ -5,20 +5,5 @@ import Generics.Pair;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-
-        Pair<String, Integer> pair = new Pair<String, Integer>("ID", 3);
-        System.out.println(pair.getFirst());
-        System.out.println(pair.getSecond());
-
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("rahul");
-        list.add("jai");
-        // list.add(32);//compile time error  
-        String s = list.get(1);// type casting is not required  
-        System.out.println("element is: " + s);
-        Iterator<String> itr = list.iterator();
-        while (itr.hasNext())
-            System.out.println(itr.next());
-
     }
 }
