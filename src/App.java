b6194b5bdb04f08e90158f7251fcb0427af5951d
MotasemZiad <miziad2000@gmail.com>
2022-11-08 10:15:35 +0200

    throwing exception..

--- src/App.java
+++ src/App.java
@@ -12,6 +12,16 @@ public class App {
             System.out.println("Try Catch Block end");
         }
 
+        // checkAge(15);
+        checkAge(20);
+
+    }
+
+    public static void checkAge(int age) {
+        if (age < 18)
+            throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+        else
+            System.out.println("Access granted - You are old enough!");
     }
 
     public static <T> void swap(T[] a, int i, int j) {
