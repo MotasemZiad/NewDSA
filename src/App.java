923b66a69b2fbaf3a32e8f7b9eb77f3a5cb3673c
MotasemZiad <miziad2000@gmail.com>
2022-11-08 10:11:19 +0200

    Exception Handling

--- src/App.java
+++ src/App.java
@@ -1,30 +1,16 @@
-import java.util.*;
-
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        ArrayList<String> list = new ArrayList<String>();
+        int[] myArray = { 1, 2, 3, 4, 5 };
 
-        list.add("Motasem");
-        list.add("Ahmed");
-        list.add("Fawzy");
-        // list.add(2); // Compile time error
-        String s = list.get(1); // Type casting is not required
-        System.out.println("The second element: " + s);
-
-        Iterator<String> itr = list.iterator();
-
-        while (itr.hasNext())
-            System.out.println(itr.next());
-
-        Integer[] arrayOfIntegers = new Integer[] { 2, 35, 23, 19, 92 };
-        Character[] arrayOfCharacters = new Character[] { 'C', 'F', 'W', 'A', 'D' };
-        sort(arrayOfIntegers);
-        sort(arrayOfCharacters);
-
-        System.out.println(Arrays.toString(arrayOfIntegers));
-        System.out.println(Arrays.toString(arrayOfCharacters));
+        try {
+            System.out.println(myArray[10]);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        } finally {
+            System.out.println("Try Catch Block end");
+        }
 
     }
 
