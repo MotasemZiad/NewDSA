fa697c098b563206ea3f2b467663f6ad156953b1
MotasemZiad <miziad2000@gmail.com>
2022-11-08 20:50:56 +0200

    binary search using recursion

--- src/App.java
+++ src/App.java
@@ -1,6 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(factorial(6));
+        int[] myArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        System.out.println(binarySearch(myArray, 2, 1, 10));
+    }
+
+    public static boolean binarySearch(int[] data, int target, int low, int high) {
+        if (low > high) {
+            return false;
+        } else {
+            int mid = (low + high) / 2;
+            if (target == data[mid]) {
+                return true;
+            } else if (target < data[mid]) {
+                return binarySearch(data, target, low, mid - 1);
+            } else {
+                return binarySearch(data, target, mid + 1, high);
+            }
+        }
     }
 
     public static int factorial(int n) throws IllegalArgumentException {
