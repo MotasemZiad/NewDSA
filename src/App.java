ee4b70e627c1917e631941b390a9ace9026b1327
MotasemZiad <miziad2000@gmail.com>
2022-11-08 09:23:03 +0200

    ArrayList & Iterators

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        ArrayList<String> list = new ArrayList<String>();
+
+        list.add("Motasem");
+        list.add("Ahmed");
+        list.add("Fawzy");
+        // list.add(2); // Compile time error
+        String s = list.get(1); // Type casting is not required
+        System.out.println("The second element: " + s);
+
+        Iterator<String> itr = list.iterator();
+
+        while (itr.hasNext())
+            System.out.println(itr.next());
+
     }
 }
